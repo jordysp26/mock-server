c7add25d80c5cb92799e0f833fcc790b79c93948
jsalas <jordysp26@gmail.com>
2024-08-19 12:00:50 -0300

    Se actualiza mock

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ConfiguracionAPPWEBMockModule } from './modules/configuracion-APPWEB-mock/configuracion-APPWEB-mock.module';
 import { CuentasAPPWEBMockModule } from './modules/cuentas-APPWEB-mock/cuentas-APPWEB-mock.module';
+import { FedaLoginMockModule } from './modules/feda-login-mock/feda-login-mock.module';
 import { FedaMockModule } from './modules/feda-mock/feda-mock.module';
 import { FirmasAPPWEBMockModule } from './modules/firmas-APPWEB-mock/firmas-APPWEB-mock.module';
 import { SfaAPPWEBMockModule } from './modules/sfa-APPWEB-mock/sfa-APPWEB-mock.module';
@@ -13,5 +14,6 @@ export const moduleMocks = [
   TransferenciasAPPWEBMockModule,
   UsuariosAPPWEBMockModule,
   FirmasAPPWEBMockModule,
-  FedaMockModule
+  FedaMockModule,
+  FedaLoginMockModule
 ];
